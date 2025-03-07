aa9a32c6017bb462353f9c30dfb9ffbe851a2c95
Niyati Aggarwal <niyatiagg4641@gmail.com>
2025-03-06 18:27:36 -0800

    Adding github repos dataset and sqllite db file

--- initdb.py
+++ initdb.py
@@ -3,7 +3,6 @@ import asyncio
 import json, models
 from tortoise import Tortoise
 
-
 async def init_db() -> None:
     await Tortoise.init(db_url='sqlite://db.sqlite3', modules={'models': ['models']})
     await Tortoise.generate_schemas()
@@ -26,9 +25,26 @@ async def load_blogs() -> None:
             stars = item.get('watchers_count', None)
         )
 
+async def load_repos() -> None:
+    with open('datasets/repos.json', 'r') as file:
+        data = json.load(file)
+    for item in data['items']:
+        if item['description'] is not None:
+            await models.Resource.create(
+                title = item['full_name'],
+                category = item.get('category', None),
+                description = item['description'],
+                image = item['owner']['avatar_url'],
+                url = item['html_url'],
+                date = item.get('date', None),
+                language = item.get('language', None),
+                stars = item.get('watchers_count', None)
+            )
+
 async def main():
     await init_db()
     await load_blogs()
+    await load_repos()
     await close_db()
 
 if __name__ == "__main__":
